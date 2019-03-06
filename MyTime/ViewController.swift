06e4c94d8e0d5e485d20d70620181182a33d27b8
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-06 10:44:58 +0200

    Initial Commit

---
+++ MyTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTime
+//
+//  Created by Артем on 3/6/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
