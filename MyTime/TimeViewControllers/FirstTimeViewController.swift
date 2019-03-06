8699ff5a5a084e1ba4f2cc57486771d4cd6a1fe3
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-03-06 10:57:34 +0200

    start -> create TabBar -> create 1,2,3 vc's

---
+++ MyTime/TimeViewControllers/FirstTimeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  FirstTimeViewController.swift
+//  MyTime
+//
+//  Created by Артем on 3/6/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class FirstTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
---
+++ MyTime/TimeViewControllers/SecondTimeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SecondTimeViewController.swift
+//  MyTime
+//
+//  Created by Артем on 3/6/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class SecondTimeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
